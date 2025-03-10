58a5edf56f28b8413f5161ee11e316b9befd0f84
filip <filipskydt@gmail.com>
2025-03-10 10:35:30 +0100

    lavet player og sat op til at lave mub

--- slutprojekt/player.gd
+++ slutprojekt/player.gd
@@ -1,5 +1,5 @@
-extends Node2D
-
+extends Area2D
+var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +8,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("højre"):
+		velocity.x += 1
+	
+	if Input.is_action_pressed("venstre"):
+		velocity.x -= 1
+	
+	if Input.is_action_pressed("ned"):
+		velocity.y += 1
+	
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+	
 	
